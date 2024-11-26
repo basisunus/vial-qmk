ad94935c6424b40940de7a583984c23405f6137e
basisunus <basisunus@yahoo.com>
2024-11-26 13:12:17 -0700

    added device enumeration

--- host/host.cpp
+++ host/host.cpp
@@ -2,7 +2,6 @@
 #include "wx/taskbar.h"
 #include "host.h"
 #include "images.h"
-#include <hidapi.h>
 
 // ----------------------------------------------------------------------------
 // global variables
@@ -22,7 +21,7 @@ wxIMPLEMENT_APP(HostApp);
 
 bool HostApp::OnInit()
 {
-    m_checker = new wxSingleInstanceChecker("MyApp");
+    m_checker = new wxSingleInstanceChecker("HostApp");
     if (m_checker->IsAnotherRunning())
     {
         wxLogError("Another instance of the application is already running.");
@@ -42,9 +41,7 @@ bool HostApp::OnInit()
     }
 
     // Create the main window
-    gs_dialog = new HostDlg("Host");
-
-    //gs_dialog->Show(true);
+    gs_dialog = new HostDlg("Settings");
 
     return true;
 }
@@ -53,6 +50,7 @@ int HostApp::OnExit()
 {
     if (m_checker)
         delete m_checker;
+
     return 0;
 }
 
@@ -61,9 +59,7 @@ int HostApp::OnExit()
 // ----------------------------------------------------------------------------
 
 wxBEGIN_EVENT_TABLE(HostDlg, wxDialog)
-EVT_BUTTON(wxID_ABOUT, HostDlg::OnAbout)
 EVT_BUTTON(wxID_OK, HostDlg::OnOK)
-EVT_BUTTON(wxID_EXIT, HostDlg::OnExit)
 EVT_CLOSE(HostDlg::OnCloseWindow)
 wxEND_EVENT_TABLE()
 
@@ -75,16 +71,10 @@ HostDlg::HostDlg(const wxString& title)
     wxSizerFlags flags;
     flags.Border(wxALL, 10);
 
-    sizerTop->Add(new wxStaticText(this, wxID_ANY, "Press 'Hide me' to hide this window, Exit to quit."), flags);
-
-    sizerTop->Add(new wxStaticText(this, wxID_ANY, "Double-click on the taskbar icon to show me again."), flags);
-
     sizerTop->AddStretchSpacer()->SetMinSize(200, 50);
 
     wxSizer* const sizerBtns = new wxBoxSizer(wxHORIZONTAL);
-    sizerBtns->Add(new wxButton(this, wxID_ABOUT, "&About"), flags);
-    sizerBtns->Add(new wxButton(this, wxID_OK, "&Hide"), flags);
-    sizerBtns->Add(new wxButton(this, wxID_EXIT, "E&xit"), flags);
+    sizerBtns->Add(new wxButton(this, wxID_OK, "&OK"), flags);
 
     sizerTop->Add(sizerBtns, flags.Align(wxALIGN_CENTER_HORIZONTAL));
     SetSizerAndFit(sizerTop);
@@ -95,104 +85,118 @@ HostDlg::HostDlg(const wxString& title)
     // we should be able to show up to 128 characters on Windows
     wxIcon icon;
     icon.CopyFromBitmap(wxGetBitmapFromMemory(squares));
-    if (!m_taskBarIcon->SetIcon(icon, "Host"))
+    if (!m_taskBarIcon->SetIcon(icon, "Ortholinear Keyboardisplay"))
     {
-        wxLogError("Could not set icon.");
+        wxMessageBox("Could not set icon.", "Error", wxOK | wxICON_ERROR);
     }
+
+    // init hidapi
+    if (hid_init())
+    {
+        wxMessageBox("HID failed to initialize.", "Error", wxOK | wxICON_ERROR);
+    }
+
+    //timer for device enumeration
+    m_dev_enum = new wxTimer(this);
+    Bind(wxEVT_TIMER, &HostDlg::OnDevEnum, this);
+    m_dev_enum->Start(5000);    //check every 5 seconds
 }
 
 HostDlg::~HostDlg() {
     delete m_taskBarIcon;
-}
 
-void HostDlg::OnAbout(wxCommandEvent& WXUNUSED(event))
-{
+    //hidapi
+    hid_exit();
+
+    //timer
+    m_dev_enum->Stop();
+    delete m_dev_enum;
+
+    close_all();
 }
 
 void HostDlg::OnOK(wxCommandEvent& WXUNUSED(event)) {
     Show(false);
 }
 
-void HostDlg::OnExit(wxCommandEvent& WXUNUSED(event)) {
-    Close(true);
-}
-
 void HostDlg::OnCloseWindow(wxCloseEvent& WXUNUSED(event)) {
-    Destroy();
+    Show(false);
 }
 
-// ----------------------------------------------------------------------------
+void HostDlg::OnDevEnum(wxTimerEvent& event)
+{
+    std::map<std::string, bool> current_devices;
+    struct hid_device_info *devs, *cur_dev;
+
+    devs = hid_enumerate(0x0, 0x0);
+    cur_dev = devs;
+    while (cur_dev)
+    {
+        if (is_supported(cur_dev->vendor_id, cur_dev->product_id))
+        {
+            std::string key = get_key(cur_dev->vendor_id, cur_dev->product_id);
+            current_devices[key] = true;
+
+            if (m_keyboards.find(key) == m_keyboards.end())
+            {
+                //open new device
+                hid_device* handle = hid_open(cur_dev->vendor_id, cur_dev->product_id, NULL);
+                if (handle)
+                {
+                    Keyboard kb =
+                    {
+                        cur_dev->vendor_id,
+                        cur_dev->product_id,
+                        handle,
+                        KB_NONE,
+                        0
+                    };
+                    m_keyboards.insert(std::pair<std::string, Keyboard>(key, kb));
+                }
+            }
+        }
+        cur_dev = cur_dev->next;
+    }
+    hid_free_enumeration(devs);
+
+    //close devices that are no longer connected
+    for (auto it = m_keyboards.begin(); it != m_keyboards.end();)
+    {
+        if (current_devices.find(it->first) == current_devices.end())
+        {
+            hid_close(it->second.hd);
+            it = m_keyboards.erase(it);
+        }
+        else
+            ++it;
+    }
+}
+    // ----------------------------------------------------------------------------
 // HostTrayIcon implementation
 // ----------------------------------------------------------------------------
 
-enum { PU_RESTORE = 10001, PU_NEW_ICON, PU_EXIT, PU_CHECKMARK, PU_SUB1, PU_SUB2, PU_SUBMAIN };
+enum { PU_SETTINGS = 10001, PU_EXIT };
 
 wxBEGIN_EVENT_TABLE(HostTrayIcon, wxTaskBarIcon)
-EVT_MENU(PU_RESTORE, HostTrayIcon::OnMenuRestore)
+EVT_MENU(PU_SETTINGS, HostTrayIcon::OnMenuSettings)
 EVT_MENU(PU_EXIT, HostTrayIcon::OnMenuExit)
-EVT_MENU(PU_NEW_ICON, HostTrayIcon::OnMenuSetNewIcon)
-EVT_MENU(PU_CHECKMARK, HostTrayIcon::OnMenuCheckmark)
-EVT_UPDATE_UI(PU_CHECKMARK, HostTrayIcon::OnMenuUICheckmark)
 EVT_TASKBAR_LEFT_DCLICK(HostTrayIcon::OnLeftButtonDClick)
-EVT_MENU(PU_SUB1, HostTrayIcon::OnMenuSub)
-EVT_MENU(PU_SUB2, HostTrayIcon::OnMenuSub)
 wxEND_EVENT_TABLE()
 
-void HostTrayIcon::OnMenuRestore(wxCommandEvent&) {
+void HostTrayIcon::OnMenuSettings(wxCommandEvent&) {
     gs_dialog->Show(true);
 }
 
 void HostTrayIcon::OnMenuExit(wxCommandEvent&) {
-    gs_dialog->Close(true);
-}
-
-static bool check = true;
-
-void HostTrayIcon::OnMenuCheckmark(wxCommandEvent&) {
-    check = !check;
-}
-
-void HostTrayIcon::OnMenuUICheckmark(wxUpdateUIEvent& event) {
-    event.Check(check);
-}
-
-void HostTrayIcon::OnMenuSetNewIcon(wxCommandEvent&) {
-    // wxIcon icon(smile_xpm);
-
-    // if (!SetIcon(wxBitmapBundle::FromBitmaps(
-    //                  wxBitmap(smile_xpm),
-    //                  wxBitmap(smile2_xpm)
-    //              ),
-    //              "wxTaskBarIcon Sample - a different icon"))
-    //     wxMessageBox("Could not set new icon.");
-}
-
-void HostTrayIcon::OnMenuSub(wxCommandEvent&) {
-    wxMessageBox("You clicked on a submenu!");
+    gs_dialog->Destroy();
 }
 
 // Overridables
 wxMenu* HostTrayIcon::CreatePopupMenu() {
     wxMenu* menu = new wxMenu;
-    menu->Append(PU_RESTORE, "&Restore main window");
+    menu->Append(PU_SETTINGS, "&Settings");
     menu->AppendSeparator();
-    menu->Append(PU_NEW_ICON, "&Set New Icon");
-    menu->AppendSeparator();
-    menu->AppendCheckItem(PU_CHECKMARK, "Test &check mark");
-    menu->AppendSeparator();
-    wxMenu* submenu = new wxMenu;
-    submenu->Append(PU_SUB1, "One submenu");
-    submenu->AppendSeparator();
-    submenu->Append(PU_SUB2, "Another submenu");
-    menu->Append(PU_SUBMAIN, "Submenu", submenu);
-    /* OSX has built-in quit menu for the dock menu, but not for the status item */
-#ifdef __WXOSX__
-    if (OSXIsStatusItem())
-#endif
-    {
-        menu->AppendSeparator();
-        menu->Append(PU_EXIT, "E&xit");
-    }
+    menu->Append(PU_EXIT, "E&xit");
     return menu;
 }
 
--- host/host.h
+++ host/host.h
@@ -1,6 +1,31 @@
 #include "wx/wx.h"
 #include <wx/mstream.h>
 #include <wx/snglinst.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <hidapi.h>
+
+enum kb_mode
+{
+    KB_NONE = 0,
+    KB_LAYER,
+    KB_TIME
+};
+
+struct Keyboard
+{
+    uint16_t vendor_id;
+    uint16_t product_id;
+    hid_device *hd;
+    kb_mode mode;
+    wxTimer* timer;
+};
+
+static std::vector<Keyboard> supported_keyboards = {
+    {0x8065, 0x8048, 0, KB_NONE, 0}, // pabile p48
+    {0x594D, 0x0075, 0, KB_NONE, 0}  // ymdk id75
+};
 
 class HostTrayIcon : public wxTaskBarIcon
 {
@@ -14,12 +39,8 @@ public:
     {}
 
     void OnLeftButtonDClick(wxTaskBarIconEvent&);
-    void OnMenuRestore(wxCommandEvent&);
+    void OnMenuSettings(wxCommandEvent&);
     void OnMenuExit(wxCommandEvent&);
-    void OnMenuSetNewIcon(wxCommandEvent&);
-    void OnMenuCheckmark(wxCommandEvent&);
-    void OnMenuUICheckmark(wxUpdateUIEvent&);
-    void OnMenuSub(wxCommandEvent&);
     virtual wxMenu *CreatePopupMenu() wxOVERRIDE;
 
     wxDECLARE_EVENT_TABLE();
@@ -41,11 +62,47 @@ public:
     HostDlg(const wxString& title);
     virtual ~HostDlg();
 
+private:
+    wxTimer *m_dev_enum;
+    std::map<std::string, Keyboard> m_keyboards;//connected keyboards
+
+private:
+    std::string get_key(uint16_t vendor_id, uint16_t product_id)
+    {
+     return std::to_string(vendor_id) + ":" + std::to_string(product_id);
+    }
+
+    void close_all()
+    {
+        for (auto& kb : m_keyboards)
+        {
+            hid_close(kb.second.hd);
+            if (kb.second.timer)
+            {
+                kb.second.timer->Stop();
+                delete kb.second.timer;
+            }
+        }
+        m_keyboards.clear();
+    }
+
+    bool is_supported(uint16_t vendor_id, uint16_t product_id)
+    {
+        for (const auto &kb : supported_keyboards)
+        {
+            if (kb.vendor_id == vendor_id &&
+                kb.product_id == product_id)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 protected:
-    void OnAbout(wxCommandEvent& event);
     void OnOK(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
     void OnCloseWindow(wxCloseEvent& event);
+    void OnDevEnum(wxTimerEvent &event);
 
     HostTrayIcon   *m_taskBarIcon;
 #if defined(__WXOSX__) && wxOSX_USE_COCOA
